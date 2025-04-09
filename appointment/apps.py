9591e2bf1ef2907b228db9065e6ffe02a2814166
mabdulaziz3 <abdulazizmurodov03@gmail.com>
2025-04-10 03:27:07 +0500

    Initial of the django barbershop project

---
+++ appointment/apps.py
@@ -0,0 +1,9 @@
+# apps.py
+# Path: appointment/apps.py
+
+from django.apps import AppConfig
+
+
+class AppointmentConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "appointment"
